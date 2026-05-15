0a605d19b5f4719d6d58f5ad58d0a8c9dbd54c27
Boiketlo20 <nzimandeyolanda058@gmail.com>
2026-05-15 05:39:28 +0200

    Start with Express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes/index')
+
+const port = process.env.PORT || 3000 ;
+
+app.use('/', routes);
+
+app.listen(port, () =>{
+    console.log("Listening on port 3000")
+})
